15300fc57679716fb36782111d6894481b2f722c
Tamás Gulácsi <T.Gulacsi@unosoft.hu>
2024-06-07 14:11:31 +0200

    record changes with Installed()

--- yb.go
+++ yb.go
@@ -2,11 +2,13 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
+// Package yb contains some helpers and wraps goyek/goyek/v2.
 package yb
 
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"go/build"
 	"io/fs"
 	"log/slog"
@@ -14,6 +16,7 @@ import (
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"sync"
 
 	"github.com/goyek/goyek/v2"
 	"github.com/goyek/x/boot"
@@ -30,8 +33,12 @@ var (
 	Define     = goyek.Define
 	Main       = boot.Main
 	SetDefault = goyek.SetDefault
+
+	installedMu sync.RWMutex
+	installed   = make(map[string]struct{})
 )
 
+// GoDeps returns the subpackage names that the given package depends on.
 func GoDeps(ctx context.Context, name string) []string {
 	pkg, err := build.ImportDir("./"+name, build.IgnoreVendor)
 	if err != nil {
@@ -60,21 +67,54 @@ func init() {
 	}
 }
 
-func GoInstall(a *goyek.A, force bool) bool {
-	a.Helper()
-	ctx := ContextWithA(a)
-	if old, err := QtcIsOld(ctx, a.Name()); err != nil {
-		a.Error(err)
-	} else if old {
-		Run(a, []string{"qtc"}, AtDir(a.Name()))
+// Installed returns the list of succeessfully installed names.
+func Installed() []string {
+	installedMu.RLock()
+	defer installedMu.RUnlock()
+	ss := make([]string, 0, len(installed))
+	for k := range installed {
+		ss = append(ss, k)
 	}
-	if GoShouldBuild(ctx, a.Name()) {
-		Run(a, []string{"go", "install", "-ldflags=-s -w", "-tags=" + brunoCus, "./" + a.Name()})
-		return true
-	}
-	return false
+	return ss
 }
 
+// ResetInstalled empties the installed set.
+func ResetInstalled() { installedMu.Lock(); clear(installed); installedMu.Unlock() }
+
+// GoInstall go install the given name.
+func GoInstall(ctx context.Context, name string, force bool) (bool, error) {
+	if old, err := QtcIsOld(ctx, name); err != nil {
+		return true, err
+	} else if old || force {
+		cmd := exec.CommandContext(ctx, "qtc")
+		cmd.Dir = name
+		if b, err := cmd.CombinedOutput(); err != nil {
+			return true, fmt.Errorf("%s: %w", string(b), err)
+		}
+	}
+	if force || GoShouldBuild(ctx, name) {
+		cmd := exec.CommandContext(ctx, "go", "install", "-ldflags=-s -w", "-tags="+brunoCus, "./"+name)
+		if b, err := cmd.CombinedOutput(); err != nil {
+			return true, fmt.Errorf("%s: %w", string(b), err)
+		}
+		installedMu.Lock()
+		installed[name] = struct{}{}
+		installedMu.Unlock()
+		return true, nil
+	}
+	return false, nil
+
+}
+
+// GoInstallA is GoInstall for a.Name() with a.Context().
+func GoInstallA(a *goyek.A, force bool) {
+	a.Helper()
+	if _, err := GoInstall(ContextWithA(a), a.Name(), force); err != nil {
+		a.Fatal(err)
+	}
+}
+
+// MTime returns the maximum ModTime().UnixMillis() of the given paths.
 func MTime(paths ...string) int64 {
 	var maxTime int64
 	for _, fn := range paths {
@@ -89,6 +129,7 @@ func MTime(paths ...string) int64 {
 	return maxTime
 }
 
+// GoShouldBuild reports whether the given package needs compilation.
 func GoShouldBuild(ctx context.Context, name string) bool {
 	logger := LoggerFromContext(ctx)
 	if old, err := QtcIsOld(ctx, name); err != nil {
@@ -122,6 +163,7 @@ func GoShouldBuild(ctx context.Context, name string) bool {
 	return false
 }
 
+// QtcIsOld reports whether the given directory needs qtc to be run.
 func QtcIsOld(ctx context.Context, root string) (bool, error) {
 	logger := LoggerFromContext(ctx)
 	var old bool
@@ -153,6 +195,7 @@ func QtcIsOld(ctx context.Context, root string) (bool, error) {
 	return old, err
 }
 
+// Run an external program reporting on a.
 func Run(a *goyek.A, progArgs []string, runOptions ...runOption) {
 	cmd := exec.CommandContext(a.Context(), progArgs[0], progArgs[1:]...)
 	for _, o := range runOptions {
@@ -167,8 +210,10 @@ func Run(a *goyek.A, progArgs []string, runOptions ...runOption) {
 
 type runOption func(*exec.Cmd)
 
+// AtDir runOption sets cmd.Dir.
 func AtDir(dir string) runOption { return func(cmd *exec.Cmd) { cmd.Dir = dir } }
 
+// ReadDirLinks reads the links contained at path dir.
 func ReadDirLinks(path string) ([]string, error) {
 	dis, err := os.ReadDir(path)
 	var links []string
@@ -178,6 +223,7 @@ func ReadDirLinks(path string) ([]string, error) {
 	return links, err
 }
 
+// PackageName returns the name of the package at the given path.
 func PackageName(path string) (string, error) {
 	pkg, err := build.ImportDir(path, build.IgnoreVendor)
 	if err != nil {
@@ -188,9 +234,12 @@ func PackageName(path string) (string, error) {
 
 type ctxA struct{}
 
+// ContextWithA returns a.Context()
 func ContextWithA(a *A) context.Context {
 	return context.WithValue(a.Context(), ctxA{}, a)
 }
+
+// AFromContext returns A from the Context.
 func AFromContext(ctx context.Context) *A { a, _ := ctx.Value(ctxA{}).(*A); return a }
 
 type aLogger interface {
@@ -200,9 +249,12 @@ type aLogger interface {
 
 type ctxLog struct{}
 
+// ComtextWithLogger returns a context with the logger set.
 func ContextWithLogger(ctx context.Context, logger aLogger) context.Context {
 	return context.WithValue(ctx, ctxLog{}, logger)
 }
+
+// LoggerFromContext returns the logger from the context, or a default slog logger.
 func LoggerFromContext(ctx context.Context) aLogger {
 	if logger, ok := ctx.Value(ctxLog{}).(aLogger); ok {
 		return logger
